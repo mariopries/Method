63e0de2612695c1a00b0845c290a342680a68b01
Lucas Martins Prestini <37966787+Prestini@users.noreply.github.com>
2018-07-27 16:57:02 -0300

    Remoção de comentários de teste

--- js/method_posto.js
+++ js/method_posto.js
@@ -10,23 +10,12 @@ window.onload = () => {
 class Method_Scripts {
     constructor() {
         this.scripts = [
-            //-- Bibliotecas externas
-            // "shared/method/js/jquery.cropit.js",
-            // "shared/method/js/jquery.mask.js",
-            // "shared/method/js/xls-export.js",
-            // //--------------------------------
-            // //-- Funções Custom Method
-            // "shared/method/js/method_loading.js"
-            // //--------------------------------
-            // //-- Arquivos Externos
-            // "shared/method/js/method_menu.js",
-            // "shared/method/js/method_mobile.js",
-            // "shared/method/js/method_style.js",
-            // "shared/method/js/pnotify.custom.min.js",
-            // "shared/method/js/method_mask.js",
-            // "shared/method/js/method_onload.js",
-            // "shared/method/js/method_excel.js"
+            // -- Bibliotecas externas
             //--------------------------------
+            //-- Funções Custom Method
+            //--------------------------------
+            //-- Arquivos Externos
+            // --------------------------------
         ];
     }
 
--- js/method_wauditor.js
+++ js/method_wauditor.js
@@ -6,26 +6,20 @@ window.onload = () => {
     Files = Files.filter(value => value);
     Scripts.AppendScripts();
 };
-//--Testessss
+
 class Method_Scripts {
     constructor() {
         this.scripts = [
             //-- Bibliotecas externas
-            // "shared/method/js/jquery.cropit.js",
-            // "shared/method/js/jquery.mask.js",
-            // "shared/method/js/xls-export.js",
-            // //--------------------------------
-            // //-- Funções Custom Method
-            // "shared/method/js/method_loading.js"
-            // //--------------------------------
-            // //-- Arquivos Externos
-            // "shared/method/js/method_menu.js",
-            // "shared/method/js/method_mobile.js",
-            // "shared/method/js/method_style.js",
-            // "shared/method/js/pnotify.custom.min.js",
-            // "shared/method/js/method_mask.js",
-            // "shared/method/js/method_onload.js",
-            // "shared/method/js/method_excel.js"
+
+            //--------------------------------
+
+            //-- Funções Custom Method
+
+            //--------------------------------
+
+            //-- Arquivos Externos
+            
             //--------------------------------
         ];
     }
