a999901a4896b6581fe481bc42115d6cfe48439f
Lucas Martins Prestini <37966787+Prestini@users.noreply.github.com>
2018-07-30 13:50:23 -0300

    Adição do avatar.png e do dropdown menu na barra de config

--- js/method_style.js
+++ js/method_style.js
@@ -34,24 +34,61 @@ class Config {
         let sideConfig = document.createElement("div");
         sideConfig.className = "sidebar-config expanded";
         sideConfig.id = "config-bar";
-        sideConfig.innerHTML = this.BuildSwitch();
+        sideConfig.innerHTML = this.Build();
         document.body.appendChild(sideConfig);
+        this.ButtonToggle('btnConfig1');
     }
 
-    BuildSwitch() {
+    Build() {
         let rawHTML = "<ul>";
         for (let i = 0; i < this.items.length; i++) {
-            rawHTML += `<li>
-                <i class="menu-icon ${this.items[i].icon}"></i>
-                <span>&ThickSpace;${this.items[i].title}</span>
+            rawHTML += '<li>' + this.BuildSwitch(this.items[i]) + '</li>';
+        }
+        rawHTML += '<li>' + this.BuildCombo({icon: "fas fa-cog", title: 'Config 6'}) + '</li>';
+        rawHTML += "</ul>";
+        return rawHTML;
+    }
+
+    BuildSwitch(item) {
+        return `<i class="menu-icon ${item.icon}"></i>
+                <span>&ThickSpace;${item.title}</span>
                 <label class="switch switch-label switch-pill switch-success switch-sm float-right config-switch">
                     <input type="checkbox" class="switch-input" checked>
                     <span class="switch-slider" data-checked="On" data-unchecked="Off"></span>
-              </label>
-            </li>`;
-        }
-        rawHTML += "</ul>";
-        return rawHTML;
+                </label>`;
+    }
+
+    BuildCombo(item) {
+        return `<i class="menu-icon ${item.icon}"></i>
+                <span>&ThickSpace;${item.title}</span>
+                <div class="btn-group" dropdown placement="bottom right" ng-reflect-placement="bottom right">
+                    <button id="btnConfig1" class="btn btn-success dropdown-toggle config-btn" dropdowntoggle="" type="button" aria-haspopup="true" aria-expanded="false">
+                        Grupo
+                        <span class="caret"></span>
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-right" role="menu" style="left: auto; right: 0px;">
+                        <li role="menuitem">
+                            <a class="dropdown-item" href="#">Action</a>
+                        </li>
+                        <li role="menuitem">
+                            <a class="dropdown-item" href="#">Another action</a>
+                        </li>
+                        <li role="menuitem">
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </li>
+                    </ul>
+                </div>`
+    }
+
+    ButtonToggle(buttonId) {
+        document.getElementById(buttonId).addEventListener('click', () => {
+            let dropdown = document.getElementById(buttonId).nextElementSibling;
+            if (dropdown.style.display !== 'block') {
+                dropdown.style.setProperty('display', 'block');
+            } else {
+                dropdown.style.setProperty('display', 'none');
+            }
+        })
     }
 
     Trigger() {
