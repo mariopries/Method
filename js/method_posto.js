6201a5e2e4de50521460fd5fb5f9c471249ccbf9
Loganit <mario.hcsc@hotmail.com>
2018-07-31 14:23:32 -0300

    correção

--- js/method_posto.js
+++ js/method_posto.js
@@ -1,9 +1,11 @@
 window.onload = () => {
     let Scripts = new Method_Scripts();
     let Styles = new Method_Styles();
+    Files[1] = Files[1].map(Styles.RemoveStyles, Styles);
+    Files[1] = Files[1].filter(value => value);
     Styles.AppendStyles();
-    Files = Files.map(Scripts.RemoveScripts, Scripts);
-    Files = Files.filter(value => value);
+    Files[0] = Files[0].map(Scripts.RemoveScripts, Scripts);
+    Files[0] = Files[0].filter(value => value);
     Scripts.AppendScripts();
 };
 
@@ -17,9 +19,7 @@ class Method_Scripts {
 
     RemoveScripts(value) {
         let achou = false;
-        console.log(value);
         for (let i = 0; i < this.scripts.length; i++) {
-            console.log("filtro");
             if (this.scripts[i] === value) {
                 achou = true;
                 break;
@@ -38,10 +38,18 @@ class Method_Scripts {
 
         document.body.appendChild(footer);
 
-        for (let i = 0; i < Files.length; i++) {
+        for (let i = 0; i < Files[0].length; i++) {
             let scriptTag = document.createElement("script");
-            scriptTag.type = "text/javascript";
-            scriptTag.src = `shared/method/js/${Files[i]}.js`;
+            
+
+            if (Files[0][i] === 'method_config_bar') {
+                scriptTag.type = "module";
+            } else {
+                scriptTag.type = "text/javascript";
+            }
+            
+            scriptTag.src = `shared/method/js/${Files[0][i]}.js`;
+
             document.body.lastElementChild.appendChild(scriptTag);
         }
     }
@@ -50,15 +58,28 @@ class Method_Scripts {
 class Method_Styles {
     constructor() {
         this.styles = [
-            "shared/method/css/method_custom.css",
-            "shared/method/css/teste.css"
+            //-- Inserir os styles que não devem ser inclusos
         ];
     }
 
-    AppendStyles() {
+    RemoveStyles(value) {
+        let achou = false;
         for (let i = 0; i < this.styles.length; i++) {
+            if (this.styles[i] === value) {
+                achou = true;
+                break;
+            }
+        }
+
+        if (!achou) {
+            return value;
+        }
+    }
+
+    AppendStyles() {
+        for (let i = 0; i < Files[1].length; i++) {
             let style = document.createElement("link");
-            style.href = this.styles[i];
+            style.href = Files[1][i];
             style.type = "text/css";
             style.rel = "stylesheet";
             document.head.appendChild(style);
